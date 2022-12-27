7629a945d6213cbdde122ea76966f6e6d94eb5b4
Itay Lavi <itay.la0@gmail.com>
2022-12-27 23:14:32 +0200

    Initial version of numba correlation

--- hw2/filters.py
+++ hw2/filters.py
@@ -1,7 +1,7 @@
-from numba import cuda
-from numba import njit
+from numba import njit, cuda, prange
 import imageio
 import matplotlib.pyplot as plt
+import numpy as np
 
 def correlation_gpu(kernel, image):
     '''Correlate using gpu
@@ -18,6 +18,17 @@ def correlation_gpu(kernel, image):
     '''
     raise NotImplementedError("To be implemented")
 
+@njit
+def checkRange(image_row, image_col, i,j):
+    return (i>=0 and i < image_row and j >= 0 and j < image_col)
+
+@njit
+def getNeighbors(kernel_row, kernel_col , i , j):
+    middle_row = int((kernel_row-1)/2)
+    middle_col = int((kernel_col-1)/2)
+    return [(i+row_offset,j+col_offset) 
+            for row_offset in prange(-1* middle_row, middle_row + 1)
+            for col_offset in prange(-1* middle_col, middle_col + 1)]
 
 @njit
 def correlation_numba(kernel, image):
@@ -33,7 +44,17 @@ def correlation_numba(kernel, image):
     ------
     An numpy array of same shape as image
     '''
-    raise NotImplementedError("To be implemented")
+    new_image = np.zeros_like(image)
+    image_row = image.shape[0]
+    image_col = image.shape[1]
+    kernel_row = kernel.shape[0]
+    kernel_col = kernel.shape[1]
+    for i in prange(image_row):
+        for j in prange(image_col):
+            pixels = np.array([image[row,col] if checkRange(image_row, image_col, row,col) else 0 for (row,col) in getNeighbors(kernel_row, kernel_col, i, j)])
+            pixels = pixels.reshape(kernel.shape)
+            new_image[i,j] = sum([pixels[row,col] * kernel[row,col] for row in prange(kernel_row) for col in prange(kernel_col)])
+    return new_image
 
 
 def sobel_operator():
